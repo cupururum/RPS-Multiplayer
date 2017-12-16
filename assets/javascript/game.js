ecb3efd265230e126861f72697e2f239c5b58849
cupururum <cupururum@Vogon-2.local>
2017-12-15 22:27:30 -0800

    static

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,3 +8,24 @@ var config = {
   messagingSenderId: "952844495956"
 };
 firebase.initializeApp(config);
+
+var database = firebase.database();
+
+//first player inputs name
+  //first player is activated
+  //first player sees the choice
+  //first player waits untill the second player inputs name and start the game
+
+// secind player started the game
+  //second player waits until first player makes choise
+
+//first player made a choise
+
+//second player is activated
+  //second player makes choise
+
+// compare choises
+  // show results
+  //increment wins/losses
+  //reselt results
+  //activate first player
